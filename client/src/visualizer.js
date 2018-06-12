fa1f279fc27479659e1a148d2cdb211ed40ec2dc
dcronkite <dcronkite@gmail.com>
2018-06-11 17:25:01 -0700

    skip undefined chunks

--- client/src/visualizer.js
+++ client/src/visualizer.js
@@ -2505,7 +2505,9 @@ var Visualizer = (function($, window, undefined) {
             $.each(data.chunks, function(chunkNo, chunk) {
                 // context for sort
                 currentChunk = chunk;
-
+                if (currentChunk === undefined) {
+                    return;
+                }
                 // text rendering
                 $.each(sourceData.highlightDict || [], function(num, dd) {
                     if (chunk.text.toLowerCase().search(dd.term) !== -1) {
